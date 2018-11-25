053e482a28580a8cad94595a72fd268d75a385bb
Paul Grad <paulgrad3rd@gmail.com>
2018-11-25 01:44:07 -0800

    I'm an idiot - we can just an html attribute to pull in the palette image. Stuff left to do 1. extract palettes 2. write algorithm for rgb to hsl conversion so we can translate hues to colorblind palettes.

--- shaders.js
+++ shaders.js
@@ -2,12 +2,14 @@ module.exports = {
 	"fragment" : `
 		precision highp float;
 		uniform sampler2D u_texture;
+		uniform sampler2D u_tex0;
 		uniform vec2 u_resolution;
 
 		void main() {
 			vec2 uv = gl_FragCoord.xy / u_resolution.xy;
 			vec3 col = texture2D(u_texture, uv).rgb;
-			gl_FragColor = vec4(col, 1);
+			vec3 pal = texture2D(u_tex0, vec2(uv.x, .5)).rgb;
+			gl_FragColor = vec4(mix(col, pal, .5), 1);
 		}
 	`
 };
